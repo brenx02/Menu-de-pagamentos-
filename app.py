e0913b2b3fd5a15ca332e2f889e5cfb93519bd6b
brenx02 <souzabreno527@gmail.com>
2026-03-18 22:20:27 -0300

    Calculo imc

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
+peso = float(input('qual seu peso? '))
+altura = float(input('qual sua altura? '))
 
+imc = peso / (altura ** 2)
 
-n1 = int(input('Digite o primeiro valor: '))
-n2 = int(input('Digite o segundo valor: '))
-opcao = 0 
+if imc < 18.5:
+    print(f'ABAIXO DO PESO, seu imc é {imc:.1f}')
 
-while opcao != 5:
-    print('''
-    [1] somar
-    [2] multiplicar
-    [3] maior
-    [4] novos numeros 
-    [5] sair do programa''')
-    opcao = int(input('>>>>>Qual a sua opção? '))
-    if opcao == 1:
-        escolha = n1 + n2
-        print(f'O valor de sua conta deu {escolha}')
-    elif opcao == 2:
-        escolha = n1 * n2
-        print(f'O valor de sua multiplicação deu {escolha}')
-    elif opcao == 3:
-        if n1 > n2:
-            print(f'O maior numero é {n1}')
-        else:
-            print(f'O maior numero é {n2}')
-    elif opcao == 4:
-        n1 = int(input('Digite o primeiro numero: '))
-        n2 = int(input('Digite o segundo numero: '))
-    elif opcao == 5:
-        print('Encerrando o programa...')
-        opcao = 5
-    else:
-        print('opção inválida, tente novamente: ') 
+elif imc < 25:
+    print(f'PESO IDEAL, seu imc é {imc:.1f}')
+
+elif imc < 30:
+    print(f'SOBREPESO, seu imc é {imc:.1f}')
+
+elif imc < 40:
+    print(f'OBESIDADE, seu imc é {imc:.1f}')
+
+else:
+    print(f'OBESIDADE MORBIDA, seu imc é {imc:.1f}')
